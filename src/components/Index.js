940741d5a97df1e1e20304adca260568a1b07f19
Uditi <uditiarora@gmail.com>
2020-01-30 21:40:36 +0530

    done with initial page

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Router from 'next/router';
 import Octicon, { MarkGithub } from '@primer/octicons-react';
-
+import '../css/index.css';
 function Index() {
+  const [username, setUsername] = useState('');
+  const handleChange = e => setUsername(e.target.value);
 
     return (
-        <main>
-          <form>
+        <div>
+          <form onSubmit={e => {
+            e.preventDefault();
+            Router.push({
+              pathname: '/user',
+              query: { id: username },
+            });
+          }}>
               <Octicon icon={MarkGithub} size="large" />
               <label htmlFor="username">Find Your Git Profile</label>
-              <input name="username" type="text"  />
+              <input name="username" type="text"  onChange={handleChange}/>
           </form>
-        </main>
+        </div>
       );
 }
 
